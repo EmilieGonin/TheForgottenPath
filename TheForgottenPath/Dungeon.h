eaf65e31c63e32408197402f056c9f29e407583b
Crimson41 <111068020+Crimson41@users.noreply.github.com>
2024-09-16 13:26:48 +0200

    add dungeon.cpp/.h

---
+++ TheForgottenPath/Dungeon.h
@@ -0,0 +1,36 @@
+#ifndef DUNGEON_H
+#define DUNGEON_H
+
+#include <vector>
+
+using std::vector;
+
+class Dungeon {
+public:
+    Dungeon();
+
+    void Display() const;
+    void MarkValidMoves(int hero_x, int hero_y);
+
+private:
+    static const int kWidth = 105;
+    static const int kHeight = 40;
+
+    static const char kEmpty = '.';
+    static const char kHero = '@';
+    static const char kGolem = 'G';
+    static const char kSpectre = 'S';
+    static const char kFaucheur = 'F';
+    static const char kWall = '#';
+    static const char kValidMove = '*';
+
+    vector<vector<char>> grid;
+    int hero_x_;
+    int hero_y_;
+
+    void Initialize();
+    void PlaceWalls();
+    void PlaceMonsters();
+};
+
+#endif
