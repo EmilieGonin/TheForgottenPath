8b263dda662ff1a9df7a4c4b66dc0c0ab5da02eb
Emilie Gonin <emilie.yuu@gmail.com>
2024-09-19 18:26:49 +0200

    fix typo

--- TheForgottenPath/GameManager.h
+++ TheForgottenPath/GameManager.h
@@ -18,7 +18,7 @@ public:
 	Player* GetPlayer() const { return m_player; }
 	vector<Monster*> GetMonsters() const { return m_monsters; }
 	int GetCurrentLevel() const { return m_currentLevel; }
-	int GetLevelsRemaning() const { return m_levelsRemaining; }
+	int GetLevelsRemaining() const { return m_levelsRemaining; }
 
 	bool GameIsOver() const { return m_levelsRemaining <= 0; }
 
